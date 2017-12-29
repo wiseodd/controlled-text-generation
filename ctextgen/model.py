1. Append lantent code to decoder input 2. Revamp test codes 3. Fix KL annealing

ctextgen/model.py
@@ -48,7 +48,7 @@ class RNN_VAE(nn.Module):
         self.q_logvar = nn.Linear(h_dim, z_dim)
 
         # Decoder
-        self.decoder = nn.GRU(self.emb_dim, z_dim+c_dim)
+        self.decoder = nn.GRU(self.emb_dim+z_dim+c_dim, z_dim+c_dim, dropout=0.3)
         self.decoder_fc = nn.Linear(z_dim+c_dim, n_vocab)
 
         # Discriminator
@@ -128,9 +128,8 @@ class RNN_VAE(nn.Module):
 
         # 1 x mbsize x (z_dim+c_dim)
         init_h = torch.cat([z.unsqueeze(0), c.unsqueeze(0)], dim=2)
-
         inputs_emb = self.word_emb(dec_inputs)  # seq_len x mbsize x emb_dim
-        # inputs_emb = torch.cat([inputs_emb, init_h.repeat(seq_len, 1, 1)], 2)
+        inputs_emb = torch.cat([inputs_emb, init_h.repeat(seq_len, 1, 1)], 2)
 
         outputs, _ = self.decoder(inputs_emb, init_h)
         seq_len, mbsize, _ = outputs.size()
@@ -221,14 +220,16 @@ class RNN_VAE(nn.Module):
 
         return X_gen
 
-    def sample_sentence(self, z, c, stochastic=True, raw=False):
+    def sample_sentence(self, z, c, stochastic=True, raw=False, temp=1):
         self.eval()
 
         word = torch.LongTensor([self.START_IDX])
         word = word.cuda() if self.gpu else word
         word = Variable(word)  # '<start>'
 
-        h = torch.cat([z.view(1, 1, -1), c.view(1, 1, -1)], dim=2)
+        z, c = z.view(1, 1, -1), c.view(1, 1, -1)
+
+        h = torch.cat([z, c], dim=2)
 
         if not isinstance(h, Variable):
             h = Variable(h)
@@ -240,11 +241,11 @@ class RNN_VAE(nn.Module):
 
         for i in range(self.MAX_SENT_LEN):
             emb = self.word_emb(word).view(1, 1, -1)
-            # emb = torch.cat([emb, z], 2)
+            emb = torch.cat([emb, z, c], 2)
 
             output, h = self.decoder(emb, h)
             y = self.decoder_fc(output).view(-1)
-            y = F.softmax(y, dim=0)
+            y = F.softmax(y/temp, dim=0)
 
             if stochastic:
                 idx = torch.multinomial(y)
@@ -289,13 +290,15 @@ class RNN_VAE(nn.Module):
     def sample_soft_embed(self, z, c, temp=1):
         self.eval()
 
+        z, c = z.view(1, 1, -1), c.view(1, 1, -1)
+
         word = torch.LongTensor([self.START_IDX])
         word = word.cuda() if self.gpu else word
         word = Variable(word)  # '<start>'
         emb = self.word_emb(word).view(1, 1, -1)
-        # emb = torch.cat([emb, z], 2)
+        emb = torch.cat([emb, z, c], 2)
 
-        h = torch.cat([z.view(1, 1, -1), c.view(1, 1, -1)], dim=2)
+        h = torch.cat([z, c], dim=2)
 
         if not isinstance(h, Variable):
             h = Variable(h)
@@ -313,9 +316,11 @@ class RNN_VAE(nn.Module):
             # w * y = n_vocab x emb_dim * n_vocab x 1
             emb = (self.word_emb.weight * y.unsqueeze(1)).sum(0).view(1, 1, -1)
 
-            assert tuple(emb.size()) == (1, 1, self.emb_dim)
-
+            # Save resulting soft embedding
             outputs.append(emb.view(1, -1))
+
+            # Append with z and c for the next input
+            emb = torch.cat([emb, z, c], 2)
 
         # 1 x 16 x emb_dim
         outputs = torch.cat(outputs, dim=0).unsqueeze(0)

test.py
@@ -24,17 +24,19 @@ parser.add_argument('--gpu', default=False, action='store_true',
                     help='whether to run in the GPU')
 parser.add_argument('--sentiment', default='positive', metavar='',
                     help='choose the generated sentiment: {`positive`, `negative`}, (default: `positive`)')
+parser.add_argument('--model', default='ctextgen', metavar='',
+                    help='choose the model: {`vae`, `ctextgen`}, (default: `ctextgen`)')
 
 args = parser.parse_args()
 
 
 mb_size = 32
-z_dim = 100
-h_dim = 128
+z_dim = 20
+h_dim = 64
 lr = 1e-3
-lr_decay_every = 5000
+lr_decay_every = 1000000
 n_iter = 20000
-log_interval = 200
+log_interval = 1000
 z_dim = h_dim
 c_dim = 2
 
@@ -49,42 +51,53 @@ model = RNN_VAE(
 )
 
 if args.gpu:
-    model.load_state_dict(torch.load('models/ctextgen.bin'))
+    model.load_state_dict(torch.load('models/{}.bin'.format(args.model)))
 else:
-    model.load_state_dict(torch.load('models/ctextgen.bin', map_location=lambda storage, loc: storage))
+    model.load_state_dict(torch.load('models/{}.bin'.format(args.model), map_location=lambda storage, loc: storage))
 
 # Samples latent and conditional codes randomly from prior
 z = model.sample_z_prior(1)
 c = model.sample_c(1)
 
-if args.sentiment == 'positive':
-    c[0, 0], c[0, 1] = 1, 0
-else:
-    c[0, 0], c[0, 1] = 0, 1
+# Generate positive sample given z
+c[0, 0], c[0, 1] = 1, 0
 
 _, c_idx = torch.max(c, dim=1)
+sample_idxs = model.sample_sentence(z, c, stochastic=True, temp=0.1)
+
 print('\nSentiment: {}'.format(dataset.idx2label(int(c_idx))))
+print('Generated: {}'.format(dataset.idxs2sentence(sample_idxs)))
 
-sample_idxs = model.sample_sentence(z, c, stochastic=False)
-print('Reconstruction (MAP): {}'.format(dataset.idxs2sentence(sample_idxs)))
+# Generate negative sample from the same z
+c[0, 0], c[0, 1] = 0, 1
 
-sample_idxs = model.sample_sentence(z, c, stochastic=True)
-print('Reconstruction (Sampling): {}'.format(dataset.idxs2sentence(sample_idxs)))
+_, c_idx = torch.max(c, dim=1)
+sample_idxs = model.sample_sentence(z, c, stochastic=True, temp=0.8)
+
+print('\nSentiment: {}'.format(dataset.idx2label(int(c_idx))))
+print('Generated: {}'.format(dataset.idxs2sentence(sample_idxs)))
 
 print()
 
 # Interpolation
-z1 = Variable(torch.zeros(z_dim) - 3).view(1, 1, z_dim)
-z2 = Variable(torch.zeros(z_dim) + 3).view(1, 1, z_dim)
 c = model.sample_c(1)
+
+z1 = model.sample_z_prior(1).view(1, 1, z_dim)
+z1 = z1.cuda() if args.gpu else z1
+
+z2 = model.sample_z_prior(1).view(1, 1, z_dim)
+z2 = z2.cuda() if args.gpu else z2
 
 # Interpolation coefficients
 alphas = np.linspace(0, 1, 5)
 
+print('Interpolation of z:')
+print('-------------------')
+
 for alpha in alphas:
     z = float(1-alpha)*z1 + float(alpha)*z2
 
-    sample_idxs = model.sample_sentence(z, c, stochastic=True)
+    sample_idxs = model.sample_sentence(z, c, stochastic=True, temp=0.1)
     sample_sent = dataset.idxs2sentence(sample_idxs)
 
     print("{}".format(sample_sent))

train_discriminator.py
@@ -13,6 +13,7 @@ from ctextgen.dataset import *
 from ctextgen.model import RNN_VAE
 
 import argparse
+from tqdm import tqdm
 
 
 parser = argparse.ArgumentParser(
@@ -28,14 +29,15 @@ args = parser.parse_args()
 
 
 mbsize = 20
-z_dim = 100
-h_dim = 128
-lr = 1e-4
+z_dim = 20
+h_dim = 64
+lr = 1e-3
 lr_decay_every = 1000000
-n_iter = 50000
-log_interval = 50
+n_iter = 5000
+log_interval = 100
 z_dim = h_dim
 c_dim = 2
+kl_weight_max = 0.4
 
 # Specific hyperparams
 beta = 0.1
@@ -43,7 +45,7 @@ lambda_c = 0.1
 lambda_z = 0.1
 lambda_u = 0.1
 
-dataset = SST_Dataset(mbsize)
+dataset = SST_Dataset(mbsize=mbsize)
 
 model = RNN_VAE(
     dataset.n_vocab, h_dim, z_dim, c_dim, p_word_dropout=0.3,
@@ -76,7 +78,7 @@ def main():
     trainer_G = optim.Adam(model.encoder_params, lr=lr)
     trainer_E = optim.Adam(model.decoder_params, lr=lr)
 
-    for it in range(n_iter):
+    for it in tqdm(range(n_iter)):
         inputs, labels = dataset.next_batch(args.gpu)
 
         """ Update discriminator, eq. 11 """
@@ -110,7 +112,7 @@ def main():
         y_z, _ = model.forward_encoder_embed(x_gen_attr.transpose(0, 1))
         y_c = model.forward_discriminator_embed(x_gen_attr)
 
-        loss_vae = recon_loss + kl_weight(it) * kl_loss
+        loss_vae = recon_loss + kl_weight_max * kl_loss
         loss_attr_c = F.cross_entropy(y_c, target_c)
         loss_attr_z = F.mse_loss(y_z, target_z)
 
@@ -124,7 +126,7 @@ def main():
         """ Update encoder, eq. 4 """
         recon_loss, kl_loss = model.forward(inputs, use_c_prior=False)
 
-        loss_E = recon_loss + kl_weight(it) * kl_loss
+        loss_E = recon_loss + kl_weight_max * kl_loss
 
         loss_E.backward()
         grad_norm = torch.nn.utils.clip_grad_norm(model.encoder_params, 5)

train_vae.py
@@ -28,11 +28,11 @@ args = parser.parse_args()
 
 
 mb_size = 32
-z_dim = 100
-h_dim = 128
+z_dim = 20
+h_dim = 64
 lr = 1e-3
 lr_decay_every = 1000000
-n_iter = 50000
+n_iter = 20000
 log_interval = 1000
 z_dim = h_dim
 c_dim = 2
@@ -46,21 +46,24 @@ model = RNN_VAE(
 )
 
 
-def kl_weight(it):
-    """
-    Credit to: https://github.com/kefirski/pytorch_RVAE/
-    """
-    return (math.tanh((it - 3500)/1000) + 1)/2
-
-
 def main():
+    # Annealing for KL term
+    kld_start_inc = 3000
+    kld_weight = 0.01
+    kld_max = 0.15
+    kld_inc = (kld_max - kld_weight) / (n_iter - kld_start_inc)
+
     trainer = optim.Adam(model.vae_params, lr=lr)
 
     for it in range(n_iter):
         inputs, labels = dataset.next_batch(args.gpu)
 
         recon_loss, kl_loss = model.forward(inputs)
-        loss = recon_loss + kl_weight(it) * kl_loss
+        loss = recon_loss + kld_weight * kl_loss
+
+        # Anneal kl_weight
+        if it > kld_start_inc and kld_weight < kld_max:
+            kld_weight += kld_inc
 
         loss.backward()
         grad_norm = torch.nn.utils.clip_grad_norm(model.vae_params, 5)